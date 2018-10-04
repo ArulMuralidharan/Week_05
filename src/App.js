14948cfecfe763a127b663ea323ecf4463d833e4
Vailrul <arulvel2@illinois.edu>
2018-10-03 20:06:27 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import update from 'react-addons-update'; // ES6
+var update = require('react-addons-update'); // ES5 with npm
+
 class App extends Component {
   state = {
     newTodo: "",
@@ -12,13 +15,64 @@ class App extends Component {
     ]
   };
 
+  handleDelete = event => {
+    var newTodoList = [...this.state.todos];
+    var index = newTodoList.indexOf(event.target.value);
+    newTodoList.splice(index, 1);
+    this.setState({todos: newTodoList});
+  };
+
+  handleChange = event => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, this.state.newTodo],
+      newTodo: ""
+    });
+  };
+
+  handleEdit = event => {
+    var newTodoList = [...this.state.todos];
+    var index = newTodoList.indexOf(event.target.value);
+    this.setState({
+      todos: update(this.state.items, {index: {name: {$set: event.target.value}}})
+    });
+  }
+
   render() {
     return (
       <div>
         <h1> How to make boba at home</h1>
         {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
+          <div>
+            <li key={todo}>
+              {todo + " "}
+              <button  value={todo} onClick={this.handleDelete}>X</button>
+
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  value={this.state.newTodo}
+                  onChange={this.handleChange}
+                />
+                <button onClick={this.handleSubmit}> + </button>
+              </form>
+
+            </li>
+          </div>
         ))}
+
+        <form onSubmit={this.handleEdit}>
+          <input
+            type="text"
+            value={this.state.newTodo}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}> + </button>
+        </form>
       </div>
     );
   }
